df13814a202c49546476c423f8f94634e608ef51
Yongjung Shin <yongshn220@gmail.com>
2023-10-26 17:41:36 -0400

    lib

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import Home from "./pages/Home";
 import ThreadView from "./pages/ThreadView";
 
@@ -8,12 +10,14 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={"Home"} component={Home}/>
-        <Tab.Screen name={"ThreadView"} component={ThreadView}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name={"Home"} component={Home}/>
+          <Tab.Screen name={"ThreadView"} component={ThreadView}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
