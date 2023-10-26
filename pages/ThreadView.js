7a943e418c438f1a021431d711e06980a1547c48
Yongjung Shin <yongshn220@gmail.com>
2023-10-26 17:28:07 -0400

    pages

---
+++ pages/ThreadView.js
@@ -0,0 +1,22 @@
+import {View, Text, StyleSheet} from "react-native";
+
+
+export default function Threads() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Threads
+      </Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
