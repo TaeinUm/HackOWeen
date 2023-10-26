27f2122b80f57b2a18f0393e9cbbd4057341fdd1
Yongjung Shin <yongshn220@gmail.com>
2023-10-26 17:38:06 -0400

    Add Navigator

--- pages/ThreadView.js
+++ pages/ThreadView.js
@@ -1,7 +1,7 @@
 import {View, Text, StyleSheet} from "react-native";
 
 
-export default function Threads() {
+export default function ThreadView() {
   return (
     <View style={styles.container}>
       <Text>
