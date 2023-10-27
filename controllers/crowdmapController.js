d7672e9dd818a2b7047e881c6a354fa22f50ad3c
Taein <taein.um@stonybrook.edu>
2023-10-27 17:11:33 -0400

    Crowd Map number updated

--- controllers/crowdmapController.js
+++ controllers/crowdmapController.js
@@ -3,11 +3,12 @@
 const CrowdMap = require('../models/CrowdMap');
 
 exports.getCrowdMapData = async (req, res) => {
-  try {
-    const data = await CrowdMap.find();
-    res.json(data);
-  } catch (err) {
-    console.error('Error fetching crowd map data:', err);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
+    try {
+        const data = await CrowdMap.find().lean();
+        res.json(data);
+    } catch (err) {
+        console.error('Error fetching crowd map data:', err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 };
+  
