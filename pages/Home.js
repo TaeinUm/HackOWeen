03e850fa531209ed4bbc27e806af2ff23facbf94
Minsuk Seo <josephseo@Minsuks-MacBook-Pro.local>
2023-10-27 03:30:10 -0400

    Save | react native maps added

--- pages/Home.js
+++ pages/Home.js
@@ -1,13 +1,30 @@
 import {View, Text, StyleSheet} from "react-native";
 import PinThread from "./PinThread";
+import MapView from 'react-native-maps';
 
 
 export default function Home() {
+  let state = {
+    mapRegion: {
+      latitude: 40.7128,
+      longitude: -74.006,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    },
+  }
+
+
+
   return(
     <View style={styles.container}>
       {/*<PinThread/>*/}
       <View style={{flex:1, backgroundColor:'red'}}>
-        <Text>123</Text>
+        <View style={styles.container}>
+          <MapView 
+            style={styles.map} 
+            initialRegion={state.mapRegion}
+          />
+        </View>
       </View>
     </View>
   )
@@ -16,12 +33,9 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display:'flex',
-    position:'absolute',
-    width:'100%',
-    height:'100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
