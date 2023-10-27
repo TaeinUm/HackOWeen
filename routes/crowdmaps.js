e31a7fe7f62378a74f7bb46baca6f859bcff7af3
Taein <taein.um@stonybrook.edu>
2023-10-27 16:29:24 -0400

    Crowd Map end point updated

--- routes/crowdmaps.js
+++ routes/crowdmaps.js
@@ -4,6 +4,6 @@ const express = require('express');
 const router = express.Router();
 const crowdMapController = require('../controllers/crowdmapController');
 
-router.get('/crowd-map', crowdMapController.getCrowdMapData);
+router.get('/', crowdMapController.getCrowdMapData);
 
 module.exports = router;
