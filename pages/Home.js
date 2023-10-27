03e5f088beb8113f156c8b10ed753214200cd385
Yongjung Shin <yongshn220@gmail.com>
2023-10-27 03:45:41 -0400

    save

--- pages/Home.js
+++ pages/Home.js
@@ -1,14 +1,22 @@
-import {View, Text, StyleSheet, Button, SafeAreaView} from "react-native";
+import {View, Button, SafeAreaView, StyleSheet} from "react-native";
 import PinThread from "./PinThread";
 import {useState} from "react";
-import {selectedThreadsAtom} from "../recoil/state";
-import {useRecoilState} from "recoil";
 import {getThreadsByLocation} from "../api/api";
+import MapView from 'react-native-maps';
 
 
 export default function Home() {
   const [showPinThread, setShowPinThread] = useState(false)
   const [selectedThreads, setSelectedThreads] = useState([])
+
+  let state = {
+    mapRegion: {
+      latitude: 40.7128,
+      longitude: -74.006,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    },
+  }
 
   async function handleOnPinClick() {
     const threads = await getThreadsByLocation("")
@@ -16,12 +24,20 @@ export default function Home() {
     setShowPinThread(!showPinThread)
   }
 
-  return(
+  return (
     <SafeAreaView style={styles.container}>
       <Button title={"Show Pin"} onPress={handleOnPinClick} ></Button>
       <View style={{flex:1, width:'100%'}}>
         {showPinThread && <PinThread threads={selectedThreads} setState={setShowPinThread}/>}
       </View>
+      {/*<View style={{flex:1}}>*/}
+      {/*  <View style={styles.container}>*/}
+      {/*    <MapView*/}
+      {/*      style={styles.map}*/}
+      {/*      initialRegion={state.mapRegion}*/}
+      {/*    />*/}
+      {/*  </View>*/}
+      {/*</View>*/}
     </SafeAreaView>
   )
 }
@@ -33,5 +49,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'white',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
